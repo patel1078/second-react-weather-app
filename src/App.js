d8c551849a83657cb821d030284c674c7a40cdef
patel1078 <jinal.patel2424@gmail.com>
2022-05-17 12:31:59 -0400

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <div>
+        <a href="https://github.com/patel1078/second-react-weather-app">
+          GitHub Repository
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
