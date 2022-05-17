853f0d42a2391fd379d85bf2160533c4ca885fb6
patel1078 <jinal.patel2424@gmail.com>
2022-05-17 12:58:08 -0400

    added portfolio link

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         This project is created by Jinal Patel. Open Source Code:
         <a
           href="https://github.com/patel1078/second-react-weather-app"
           target="_blank"
         >
+          <a
+            href="https://jinalpatel2424.wixsite.com/portfolio
+"
+            target="_blank"
+          >
+            Portfolio
+          </a>
           GitHub Repository
         </a>
       </footer>
