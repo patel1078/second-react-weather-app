47e4a0ea38ffdcf9389b48c1308ae2b5e9702dd8
patel1078 <jinal.patel2424@gmail.com>
2022-05-24 12:22:48 -0400

    added code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import "./App.css";
+import "./Weather.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Jinal Patel. Open Source Code:
-        <a
-          href="https://github.com/patel1078/second-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          GitHub Repository
-        </a>{" "}
-        And
-        <a
-          href="https://jinalpatel2424.wixsite.com/portfolio
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather defaultCity="London" />
+        <footer>
+          This project is created by Jinal Patel. Open Source Code:
+          <a
+            href="https://github.com/patel1078/second-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub Repository
+          </a>{" "}
+          And
+          <a
+            href="https://jinalpatel2424.wixsite.com/portfolio
 "
-          target="_blank"
-          rel="noreferrer"
-        >
-          Portfolio
-        </a>
-      </footer>
+            target="_blank"
+            rel="noreferrer"
+          >
+            Portfolio
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
