dc3ec05fd0a9c808a9f7c65ec9eaf84fef4882d6
patel1078 <jinal.patel2424@gmail.com>
2022-05-25 14:52:04 -0400

    CSS revision

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Weather App</h1>
-        <Weather defaultCity="London" />
-        <footer>
-          This project is created by Jinal Patel. Open Source Code:
-          <a
-            href="https://github.com/patel1078/second-react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            GitHub Repository
-          </a>{" "}
-          And
-          <a
-            href="https://jinalpatel2424.wixsite.com/portfolio
+    <body>
+      <div className="App">
+        <div className="container">
+          <h1>Weather App</h1>
+          <Weather defaultCity="New York" />
+          <footer>
+            This project is created by Jinal Patel. Open Source Code:
+            <a
+              href="https://github.com/patel1078/second-react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              GitHub Repository
+            </a>{" "}
+            And
+            <a
+              href="https://jinalpatel2424.wixsite.com/portfolio
 "
-            target="_blank"
-            rel="noreferrer"
-          >
-            Portfolio
-          </a>
-        </footer>
+              target="_blank"
+              rel="noreferrer"
+            >
+              Portfolio
+            </a>
+          </footer>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
